1559a1c5abf659337793848e7e33495f72419525
evizitei <ethan.vizitei@gmail.com>
2010-08-07 00:20:37 -0500

    clean up script
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- lib/istatus_watcher.rb
+++ lib/istatus_watcher.rb
@@ -3,19 +3,32 @@ require 'mechanize'
 class IstatusWatcher
   
   def how_many_available?
+    extract_available_medics_from(fetch_medic_page).size
+  end
+  
+  def fetch_medic_page
     agent = Mechanize.new
-    page = agent.get('http://istatus.gocolumbiamo.com/')
-    form = page.form("UserForm")
+    login(agent)
+    add_medic_page_cookie(agent)
+    agent.get("https://istatus.gocolumbiamo.com/AjaxTS2.php")
+  end
+  
+  def extract_available_medics_from(page)
+    nodes = page.parser.css("a.AVL")
+    nodes.map{|node| node.attributes["href"].value}.select{|val| val =~ /TruckID=M(11|15|21|22|23)\d/}
+  end
+  
+  def login(agent)
+    form = agent.get('http://istatus.gocolumbiamo.com/').form("UserForm")
     form.UserName = "bf.swish"
     form.Password = "1414"
-    page = agent.submit(form, form.buttons.first)
+    agent.submit(form, form.buttons.first)
+  end
+  
+  def add_medic_page_cookie(agent)
     cookie = Mechanize::Cookie.new("TScreen","4")
     cookie.secure = false
     cookie.domain = "istatus.gocolumbiamo.com"
     agent.cookie_jar.jar["istatus.gocolumbiamo.com"]["/"]["TScreen"] = cookie
-    page = agent.get("https://istatus.gocolumbiamo.com/AjaxTS2.php")
-    doc = page.parser
-    nodes = doc.css("a.AVL")
-    nodes.map{|node| node.attributes["href"].value}.select{|val| val =~ /TruckID=M\d{3}/}.size
   end
 end
