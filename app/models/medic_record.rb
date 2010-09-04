d4d20357b199de3734ade675ebf67fb008396aba
evizitei <ethan.vizitei@gmail.com>
2010-09-04 14:59:49 -0500

    set expectations correctly
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/models/medic_record.rb
+++ app/models/medic_record.rb
@@ -2,9 +2,9 @@ class MedicRecord < ActiveRecord::Base
   after_create do |record|
     prior_record = MedicRecord.find_by_id(record.id - 1)
     if !prior_record.nil?
-      if((prior_record.count == 2 and record.count == 1) or 
-         (prior_record.count == 1 and record.count == 0) or
-         ((prior_record.count == 0 or prior_record.count == 1) and record.count == 2))
+      if((prior_record.count >= 2 and record.count <= 1) or 
+         (prior_record.count >= 1 and record.count <= 0) or
+         ((prior_record.count <= 1) and record.count >= 2))
          SmsProxy.new.deliver("City Status Is #{record.count}")
       end
     end
