f594b5e24123bba1be4550ec9dee086cfc280075
evizitei <ethan.vizitei@gmail.com>
2010-09-04 15:09:37 -0500

    add josh to test list
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
   end
   
   def test_message
-    SmsProxy.new.deliver("testing from medic watcher")
+    SmsProxy.new.deliver("TEST ONLY message from medic watcher")
     flash[:notice] = "Message sent"
     redirect_to :action=>:home
   end
--- app/models/sms_proxy.rb
+++ app/models/sms_proxy.rb
@@ -1,7 +1,10 @@
 class SmsProxy
   def deliver(message)
     sms_fu = SMSFu::Client.configure(:delivery => :action_mailer)
-    sms_fu.deliver("5732395840","at&t",message)
+    [{:number=>"5732395840",:carrier=>"at&t"},
+     {:number=>"5732686228",:carrier=>"sprint"}].each do |map|
+      sms_fu.deliver("5732395840","at&t",message)
+    end
     MedicAlert.create!(:message=>message)
   end
 end
