482fdc777fa2a0c1c37f50cc13da994e65fa0910
evizitei <ethan.vizitei@gmail.com>
2010-09-04 15:15:20 -0500

    use list properly, and add the last 10 medic counts
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/models/sms_proxy.rb
+++ app/models/sms_proxy.rb
@@ -3,7 +3,7 @@ class SmsProxy
     sms_fu = SMSFu::Client.configure(:delivery => :action_mailer)
     [{:number=>"5732395840",:carrier=>"at&t"},
      {:number=>"5732686228",:carrier=>"sprint"}].each do |map|
-      sms_fu.deliver("5732395840","at&t",message)
+      sms_fu.deliver(map[:number],map[:carrier],message)
     end
     MedicAlert.create!(:message=>message)
   end
