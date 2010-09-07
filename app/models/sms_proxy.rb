291ada2ff788752bef6341a612cff83a35f8800b
evizitei <ethan.vizitei@gmail.com>
2010-09-07 17:20:57 -0500

    add new numbers to medic mailer
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/models/sms_proxy.rb
+++ app/models/sms_proxy.rb
@@ -2,7 +2,9 @@ class SmsProxy
   def deliver(message)
     sms_fu = SMSFu::Client.configure(:delivery => :action_mailer)
     [{:number=>"5732395840",:carrier=>"at&t"},
-     {:number=>"5732686228",:carrier=>"sprint"}].each do |map|
+     {:number=>"5732686228",:carrier=>"sprint"},
+     {:number=>"5733031624",:carrier=>"sprint"},
+     {:number=>"5732685942",:carrier=>"sprint"}].each do |map|
       sms_fu.deliver(map[:number],map[:carrier],message)
     end
     MedicAlert.create!(:message=>message)
