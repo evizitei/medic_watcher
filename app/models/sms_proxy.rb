7c624bea91fe4aaa6ec550f832eb5cb2c56a8eeb
evizitei <ethan.vizitei@gmail.com>
2010-09-07 19:27:18 -0500

    check times before sending in the middle of the night
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/models/sms_proxy.rb
+++ app/models/sms_proxy.rb
@@ -1,11 +1,16 @@
 class SmsProxy
   def deliver(message)
+    start_time = Time.parse("05:00:00")
+    stop_time = Time.parse("20:00:00")
+    now_time = Time.now
     sms_fu = SMSFu::Client.configure(:delivery => :action_mailer)
     [{:number=>"5732395840",:carrier=>"at&t"},
      {:number=>"5732686228",:carrier=>"sprint"},
      {:number=>"5733031624",:carrier=>"sprint"},
      {:number=>"5732685942",:carrier=>"sprint"}].each do |map|
-      sms_fu.deliver(map[:number],map[:carrier],message)
+      if now_time >= start_time and now_time <= stop_time
+        sms_fu.deliver(map[:number],map[:carrier],message)
+      end
     end
     MedicAlert.create!(:message=>message)
   end
